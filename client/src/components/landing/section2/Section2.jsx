6f648b5e1540b3926d556d239146babfb46cf826
loocia <loocia1910@gmail.com>
2022-01-24 17:48:34 +0900

    Set: landing section6 completed

--- client/src/components/landing/section2/Section2.jsx
+++ client/src/components/landing/section2/Section2.jsx
@@ -1,43 +1,6 @@
 import { useState } from 'react';
+import { categorys } from './data'
 import styles from './Section2.module.css';
-const categorys = [
-  {
-    name: '커리어',
-    img: '/img/category/career.png'
-  },
-  {
-    name: '운동/건강',
-    img: '/img/category/workout.png'
-  },
-  {
-    name: '음료/요리',
-    img: '/img/category/cooking.png'
-  },
-  {
-    name: '미술',
-    img: '/img/category/art.png'
-  },
-  {
-    name: '공예',
-    img: '/img/category/diy.png'
-  },
-  {
-    name: '음악',
-    img: '/img/category/music.png'
-  },
-  {
-    name: '사진/영상',
-    img: '/img/category/picture.png'
-  },
-  {
-    name: '재태크',
-    img: '/img/category/profits.png'
-  },
-  {
-    name: '외국어',
-    img: '/img/category/language.png'
-  }
-];
 
 
 const Section2 = () => {
--- client/src/components/landing/section6/Section6.jsx
+++ client/src/components/landing/section6/Section6.jsx
@@ -4,7 +4,7 @@ const Section6 = () => {
   return (
     <div className={styles.container}>
       <section className={styles.wrapper}>
-          <div>랜딩페이지6</div>
+        <h2 className={styles.txt}>시니어 클래스는<br/> 언제나 '시니어의 청춘'을 응원합니다.</h2>
       </section>
     </div>
   )
