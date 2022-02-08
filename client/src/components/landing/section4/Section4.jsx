3016b063ed575c7be43be21dca8d6f4e2a29d0d5
loocia <loocia1910@gmail.com>
2022-02-08 20:34:47 +0900

    Fix: section4 overflow

--- client/src/components/landing/section4/Section4.jsx
+++ client/src/components/landing/section4/Section4.jsx
@@ -2,7 +2,7 @@ import Slider from 'react-slick';
 import styles from './Section4.module.css';
 import './Section4.css'; // slick css 
 
-import { categorys } from '../section2/data';
+import { reviewImg } from './reviewData';
 
 const Section4 = () => {
 
@@ -43,16 +43,12 @@ const Section4 = () => {
               <p className={styles.subTitle}><strong>청</strong>춘은 <strong>바</strong>로 <strong>지</strong>금</p>
               <p>다른 회원님의 수강후기를 확인해보세요.</p>
               <p>회원님에 맞는 취미를 찾아보세요!</p>
-              {/* <div className={styles.slidesNav}>
-                  <div className={`${styles.slideBefore} ${styles.zIndex_10}`} ></div>
-                  <div className={`${styles.slideAfter} ${styles.zIndex_10}`} ></div>
-              </div> */}
             </div>
           </div>
           <Slider {...settings} className='slider'>
-            {categorys.map((c,idx) => 
+            {reviewImg.map((url, idx) => 
             <div key={idx} className='slides'>
-                <img src={c.img} alt={c.name} className='slide'/>
+                <img src={url} alt={url} className='slide'/>
             </div>  
             )}
           </ Slider>
---
+++ client/src/components/landing/section4/reviewData.js
@@ -0,0 +1,10 @@
+export const reviewImg = [
+    '/img/review/review2.png',
+    '/img/review/review3.png',
+    '/img/review/review4.png',
+    '/img/review/review5.png',
+    '/img/review/review6.png',
+    '/img/review/review7.png',
+    '/img/review/review8.png',
+    '/img/review/review1.png',
+];
