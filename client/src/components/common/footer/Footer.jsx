32ce98509ecb8ddd448d91231681b721126ef991
loocia <loocia1910@gmail.com>
2022-02-12 13:01:52 +0900

    Change: created date

--- client/src/components/common/footer/Footer.jsx
+++ client/src/components/common/footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
     return (
         <div className={styles.wrapper}>
             <p>개발자 : 김양현</p>
-            <p>제작기간 : 2021.12.06 ~ 2022.00.00</p>
+            <p>제작기간 : 2021.12.06 ~ 2022.02.11</p>
             <ul className={styles.iconBox}>
                 <li><a title='Notion 스터디 로그' href='https://www.notion.so/fbc0ba9fe41d4c5ba6fa9a6f74c4c975?v=bb7bd567fec4462d958f9cb8f36d3375' rel="noopener noreferrer" target="_blank"><AiOutlineBook className={styles.icon} /></a></li>
                 <li><a title='깃허브' href='https://github.com/loocia1910' rel="noopener noreferrer" target="_blank"><AiOutlineGithub className={styles.icon} /></a></li>
